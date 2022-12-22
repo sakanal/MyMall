cf325b89e5ef10b2f1dd3082270fd6dbfbc6fc8f
sakanal <sakanal9527@gmail.com>
2022-12-22 18:08:52 +0800

    解决跨域问题，双重跨域问题通过gateway网关处理

--- renren-fast/src/main/java/io/renren/config/CorsConfig.java
+++ renren-fast/src/main/java/io/renren/config/CorsConfig.java
@@ -8,11 +8,10 @@
 
 package io.renren.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
