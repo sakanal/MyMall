d84889b5f70b12e09e388156cb4dd4aec668fb56
sakanal <sakanal9527@gmail.com>
2022-12-24 16:11:55 +0800

    添加pubsub-js

--- src/views/modules/product/spuadd.vue
+++ src/views/modules/product/spuadd.vue
@@ -345,10 +345,11 @@
 
 <script>
 // 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
-// 例如：import 《组件名称》 from '《组件路径》';
+// 例如：import 《组件名称》 from '《组件路径》';
 import CategoryCascader from '../common/category-cascader'
 import BrandSelect from '../common/brand-select'
 import MultiUpload from '@/components/upload/multiUpload'
+import PubSub from 'pubsub-js'
 export default {
   // import引入的组件需要注入到对象中才能使用
   components: { CategoryCascader, BrandSelect, MultiUpload },
@@ -421,7 +422,7 @@ export default {
       inputValue: []
     }
   },
-  // 计算属性 类似于data概念
+  // 计算属性 类似于data概念
   computed: {},
   // 监控data中的数据变化
   watch: {
@@ -499,7 +500,7 @@ export default {
       row.images[index].defaultImg = 1 // 修改标志位;
       // 修改其他人的标志位
       row.images.forEach((item, idx) => {
-        if (idx != index) {
+        if (idx !== index) {
           row.images[idx].defaultImg = 0
         }
       })
@@ -508,7 +509,7 @@ export default {
       let inputValue = this.inputValue[idx].val
       if (inputValue) {
         // this.dynamicTags.push(inputValue);
-        if (this.dataResp.saleAttrs[idx].valueSelect == '') {
+        if (this.dataResp.saleAttrs[idx].valueSelect === '') {
           this.dataResp.saleAttrs[idx].valueSelect = inputValue
         } else {
           this.dataResp.saleAttrs[idx].valueSelect += ';' + inputValue
@@ -535,7 +536,7 @@ export default {
         item.forEach(attr => {
           let { attrId, attrValues, showDesc } = attr
           // 跳过没有录入值的属性
-          if (attrValues != '') {
+          if (attrValues !== '') {
             if (attrValues instanceof Array) {
               // 多个值用;隔开
               attrValues = attrValues.join(';')
@@ -590,7 +591,7 @@ export default {
         let memberPrices = []
         if (this.dataResp.memberLevels.length > 0) {
           for (let i = 0; i < this.dataResp.memberLevels.length; i++) {
-            if (this.dataResp.memberLevels[i].priviledgeMemberPrice == 1) {
+            if (this.dataResp.memberLevels[i].priviledgeMemberPrice === 1) {
               memberPrices.push({
                 id: this.dataResp.memberLevels[i].id,
                 name: this.dataResp.memberLevels[i].name,
@@ -616,7 +617,7 @@ export default {
             fullPrice: 0.0,
             reducePrice: 0.0,
             priceStatus: 0,
-            memberPrice: new Array().concat(memberPrices)
+            memberPrice: [].concat(memberPrices)
           })
         } else {
           skus.push(res)
@@ -630,7 +631,7 @@ export default {
       let res = null
       if (skus.length > 0) {
         for (let i = 0; i < skus.length; i++) {
-          if (skus[i].descar.join(' ') == descar.join(' ')) {
+          if (skus[i].descar.join(' ') === descar.join(' ')) {
             res = skus[i]
           }
         }
@@ -704,7 +705,7 @@ export default {
             method: 'post',
             data: this.$http.adornData(this.spu, false)
           }).then(({ data }) => {
-            if (data.code == 0) {
+            if (data.code === 0) {
               this.$message({
                 type: 'success',
                 message: '新增商品成功!'
@@ -728,7 +729,8 @@ export default {
     },
     // 笛卡尔积运算
     descartes (list) {
-      // parent上一级索引;count指针计数
+      let index
+// parent上一级索引;count指针计数
       var point = {}
 
       var result = []
@@ -737,7 +739,7 @@ export default {
       var temp = []
 
       // 根据参数列生成指针对象
-      for (var index in list) {
+      for (index in list) {
         if (typeof list[index] === 'object') {
           point[index] = { parent: pIndex, count: 0 }
           pIndex = index
@@ -751,7 +753,7 @@ export default {
 
       // 动态生成笛卡尔积
       while (true) {
-        for (var index in list) {
+        for (index in list) {
           tempCount = point[index]['count']
           temp.push(list[index][tempCount])
         }
@@ -779,9 +781,9 @@ export default {
       }
     }
   },
-  // 生命周期 - 创建完成（可以访问当前this实例）
+  // 生命周期 - 创建完成（可以访问当前this实例）
   created () {},
-  // 生命周期 - 挂载完成（可以访问DOM元素）
+  // 生命周期 - 挂载完成（可以访问DOM元素）
   mounted () {
     this.catPathSub = PubSub.subscribe('catPath', (msg, val) => {
       this.spu.catalogId = val[val.length - 1]
@@ -791,17 +793,17 @@ export default {
     })
     this.getMemberLevels()
   },
-  beforeCreate () {}, // 生命周期 - 创建之前
-  beforeMount () {}, // 生命周期 - 挂载之前
-  beforeUpdate () {}, // 生命周期 - 更新之前
-  updated () {}, // 生命周期 - 更新之后
+  beforeCreate () {}, // 生命周期 - 创建之前
+  beforeMount () {}, // 生命周期 - 挂载之前
+  beforeUpdate () {}, // 生命周期 - 更新之前
+  updated () {}, // 生命周期 - 更新之后
   beforeDestroy () {
     PubSub.unsubscribe(this.catPathSub)
     PubSub.unsubscribe(this.brandIdSub)
-  }, // 生命周期 - 销毁之前
-  destroyed () {}, // 生命周期 - 销毁完成
+  }, // 生命周期 - 销毁之前
+  destroyed () {}, // 生命周期 - 销毁完成
   activated () {} // 如果页面有keep-alive缓存功能，这个函数会触发
 }
 </script>
-<style scoped>
+<style scoped>
 </style>
