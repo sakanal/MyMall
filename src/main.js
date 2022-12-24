a50418fadb8bed83e53738aa21a06a0b21903b1c
sakanal <sakanal9527@gmail.com>
2022-12-24 16:42:58 +0800

    Revert "全局引入PuhSub，修改组件bug"
    This reverts commit 48a49cd1818e8d30467767f1f2500343970eb1b8.

--- src/main.js
+++ src/main.js
@@ -7,10 +7,9 @@ import '@/element-ui'                         // api: https://github.com/ElemeFE
 import '@/icons'                              // api: http://www.iconfont.cn/
 import '@/element-ui-theme'
 import '@/assets/scss/index.scss'
-import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
-import { isAuth } from '@/utils'
+import httpRequest from './utils/httpRequest' // api: https://github.com/axios/axios
+import { isAuth } from './utils'
 import cloneDeep from 'lodash/cloneDeep'
-import PubSub from 'pubsub-js'
 
 Vue.use(VueCookie)
 Vue.config.productionTip = false
@@ -23,7 +22,6 @@ if (process.env.NODE_ENV !== 'production') {
 // 挂载全局
 Vue.prototype.$http = httpRequest // ajax请求方法
 Vue.prototype.isAuth = isAuth     // 权限方法
-Vue.prototype.PubSub = PubSub   // 组件发布订阅消息
 
 // 保存整站vuex本地储存初始状态
 window.SITE_CONFIG['storeState'] = cloneDeep(store.state)
--- src/views/modules/common/brand-select.vue
+++ src/views/modules/common/brand-select.vue
@@ -58,9 +58,7 @@ export default {
     }
   },
   // 生命周期 - 创建完成（可以访问当前this实例）
-  created () {
-    this.getCatBrands()
-  },
+  created () {},
   // 生命周期 - 挂载完成（可以访问DOM元素）
   mounted () {
     // 监听三级分类消息的变化
