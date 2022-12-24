e403327fc5ca5c3a258e5dec2eb12b5c0a0591e7
sakanal <sakanal9527@gmail.com>
2022-12-24 16:09:01 +0800

    添加事务基础注解

--- common/src/main/java/com/sakanal/common/config/MybatisPlusConfig.java
+++ common/src/main/java/com/sakanal/common/config/MybatisPlusConfig.java
@@ -7,8 +7,10 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 @MapperScan("com.sakanal.**.dao")
 public class MybatisPlusConfig {
     /**
